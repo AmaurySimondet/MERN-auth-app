b5131bd68fa7c5664030d46700feb4912e62394e
AmaurySimondet <87724359+AmaurySimondet@users.noreply.github.com>
2022-09-25 19:10:59 +0200

    Delete client directory

--- client/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import API from "../utils/API.js";
-import { Outlet, Navigate } from "react-router-dom";
-
-function PrivateRoute() {
-    return API.isAuth() ? <Outlet/> : <Navigate to="/" />;
-};
-
-export default PrivateRoute;
